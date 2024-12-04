10e7bee106d65771917842473b547a635208dc0c
treetree226 <tthai2025@kehillahstudent.org>
2024-12-04 12:28:43 -0800

    aa

--- main.py
+++ main.py
@@ -4,12 +4,6 @@ import time
 
 #660x440
 
-#todo-make paramenter thingy for slide too
-
-# left, right = robot.sonars()
-# left=(-1, 1, 0.76)
-# right=(1, -1, 0.76)
-
 def stomp(direction):
     if direction== "right":
         robot.motors(1, -1, 0.76)
@@ -22,35 +16,17 @@ def stomp(direction):
         robot.motors(-1,-1,0.5)
         robot.motors(1,-1,0.76)
 
-def rightstomp(): 
-    robot.motors(1, -1, 0.76)
-    robot.motors(1,1,0.5)
-    robot.motors(-1,-1,0.5)
-    robot.motors(-1,1,0.76)
-
-def leftstomp(): 
-    robot.motors(-1, 1, 0.76)
-    robot.motors(1,1,0.5)
-    robot.motors(-1,-1,0.5)
-    robot.motors(1,-1,0.76)
-
-def slide(direction1, direction2):
-    robot.motors(direction1)
-    robot.motors(1,1,0.5)
-    robot.motors(-1,-1,0.5)
-    robot.motors(direction2)
-
-def slideleft(): 
-    robot.motors(-1, 1, 1.52)
-    robot.motors(1,1,0.5)
-    robot.motors(-1,-1,0.5)
-    robot.motors(1,-1,1.52)
-
-def slideright():
-    robot.motors(1,-1,1.52)
-    robot.motors(1,1,0.5)
-    robot.motors(-1,-1,0.5)
-    robot.motors(-1,1,1.52)
+def slide(direction):
+    if direction== "right":
+        robot.motors(1, -1, 1.52)
+        robot.motors(1,1,0.5)
+        robot.motors(-1,-1,0.5)
+        robot.motors(-1,1,1.52)
+    if direction== "left":
+        robot.motors(-1, 1, 1.52)
+        robot.motors(1,1,0.5)
+        robot.motors(-1,-1,0.5)
+        robot.motors(1,-1,1.52)
 
 def backnowyall():
     robot.motors(-1,-1,0.5)
@@ -62,24 +38,34 @@ def hop():
     robot.motors(1,1,0.25)
     robot.motors(-1,-1,0.25)
 
-def chacha():
-    robot.motors(1,-1,4)
-
 def knees():
-    robot.motors(-1, 1, 0.76)
-    robot.motors(1,1,0.5)
-    robot.motors(-1,-1,0.5)
-    robot.motors(1, -1, 0.76)
-    robot.motors(1,1,0.5)
-    robot.motors(-1,-1,0.5)
-    robot.motors(-1,1,1.52)
+    robot.motors(-1, 1, 0.25)
+    robot.motors(1,1,0.25)
+    robot.motors(-1,-1,0.25)
+    robot.motors(1, -1, 0.25)
+    robot.motors(1,1,0.25)
+    robot.motors(-1,-1,0.25)
+    robot.motors(-1, 1, 0.25)
+    robot.motors(1,1,0.25)
+    robot.motors(-1,-1,0.25)
+    robot.motors(1, -1, 0.25)
+    robot.motors(1,1,0.25)
+    robot.motors(-1,-1,0.25)
+    robot.motors(-1, 1, 0.25)
+    robot.motors(1,1,0.25)
+    robot.motors(-1,-1,0.25)
+    robot.motors(1, -1, 0.25)
 
-def funky():
+def getfunkywithit():
+    robot.motors(1,-1,2)
+
+def chacha():
     left_distance, right_distance = robot.sonars()
-    if left_distance <10:
-        pass
-    if right_distance <10:
-        pass
+    robot.sonars(-1,1,4)
+    if left_distance <5:
+        robot.sonars(-1,-1,180)
+    if right_distance <5:
+        robot.sonars(-1,-1,180)
 
 
 while True:
@@ -94,7 +80,7 @@ while True:
         left_distance, right_distance = robot.sonars()
         print(f"Left sonar is {left_distance} from the wall.")
         print(f"Right sonar is {right_distance} from the wall.")
-        slideleft()
+        slide("left")
         backnowyall()
         hop()
         hop()
@@ -103,7 +89,7 @@ while True:
         stomp("left")
         stomp("left")
         knees()
-        funky()
+        getfunkywithit()
         chacha()
         
 
--- readme.md
+++ readme.md
@@ -11,11 +11,11 @@ Project Requirements:
 * [ ] at least 5 calls to input
 * [ ] at least 5 functions
 * [ ] at least 2 functions have at least one parameter
-* [ ] at least 2 functions have return values and at least one call to the function is assigned to a variable
+* THIS ONE [ ] at least 2 functions have return values and at least one call to the function is assigned to a variable
 * [ ] each motor moves at least once
-* [ ] the robot's movement changes based on at least 5 readings of the sonar sensors**** Wait for new code update to implement this part!!
+* THIS ONE [ ] the robot's movement changes based on at least 5 readings of the sonar sensors**** Wait for new code update to implement this part!!
 * [ ] use at least 1 while loop OR recursive function call
-* [ ] at least one command (user input) causes the robot to move autonomously for at least 20 seconds
+* THIS ONE [ ] at least one command (user input) causes the robot to move autonomously for at least 20 seconds
 
 Dr. EB Todo:
 * [ ] implement sonar distance in simulator
